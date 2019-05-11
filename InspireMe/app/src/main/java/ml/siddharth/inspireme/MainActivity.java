7d668fcb31d134f14c7f45ed1e26dcea76eda216
Siddharth M <siddharth.m98@gmail.com>
2019-05-11 11:03:08 +0530

    Adding user detail page

--- InspireMe/app/src/main/java/ml/siddharth/inspireme/MainActivity.java
+++ InspireMe/app/src/main/java/ml/siddharth/inspireme/MainActivity.java
@@ -18,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                Intent intent  = new Intent(getApplicationContext(),UserDetailsActivity.class);
+                startActivity(intent);
             }
         });
 
