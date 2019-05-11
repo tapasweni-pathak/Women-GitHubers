ff83ed1bf71d15d08415ee67cd43b0eb9f210dd7
Siddharth M <siddharth.m98@gmail.com>
2019-05-11 22:17:52 +0530

    Retrived users

--- InspireMe/app/src/main/java/ml/siddharth/inspireme/MainActivity.java
+++ InspireMe/app/src/main/java/ml/siddharth/inspireme/MainActivity.java
@@ -4,10 +4,12 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView inspireme;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
